8a768ea2981ee04f441362ff8518aa49eaa733e9
Fred <domaine75@gmail.com>
2020-03-10 19:03:17 +0100

    Fix a bug when draging a picture

--- FamilyShow/ImageConverter.cs
+++ FamilyShow/ImageConverter.cs
@@ -4,32 +4,33 @@ using System.Windows.Media.Imaging;
 
 namespace Microsoft.FamilyShow
 {
-    public class ImageConverter : IValueConverter
+  public class ImageConverter : IValueConverter
+  {
+    #region IValueConverter Members
+
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
     {
-        #region IValueConverter Members
+      try
+      {
+        BitmapImage bitmap = new BitmapImage(new Uri(value.ToString()));
 
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            try
-            {
-                BitmapImage bitmap = new BitmapImage(new Uri(value.ToString()));
+        // Use BitmapCacheOption.OnLoad to prevent binding the source holding on to the photo file.
+        bitmap.CacheOption = BitmapCacheOption.OnLoad;
 
-                // Use BitmapCacheOption.OnLoad to prevent binding the source holding on to the photo file.
-                bitmap.CacheOption = BitmapCacheOption.OnLoad;
-
-                return bitmap;
-            }
-            catch
-            {
-                return "";
-            }
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            throw new NotImplementedException(Properties.Resources.NotImplemented);
-        }
-
-        #endregion
+        return bitmap;
+      }
+      catch
+      {
+        return string.Empty;
+      }
     }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      //throw new NotImplementedException(Properties.Resources.NotImplemented);
+      return new object();
+    }
+
+    #endregion
+  }
 }
