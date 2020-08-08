a8dec9f76cf05ba3354bafc0108d08fabb90cec7
Fred <domaine75@gmail.com>
2020-08-08 11:50:33 +0200

    Update copyright

--- UnitTestFamilyShow/Properties/AssemblyInfo.cs
+++ UnitTestFamilyShow/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
@@ -6,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UnitTestFamilyShow")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -14,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("a5619a18-e92c-4d06-a847-33ec178e20f3")]
 
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
