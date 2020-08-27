b38f7aeadf7c87c0552ceb1c01df6b60b0c5c689
Fred <domaine75@gmail.com>
2020-08-27 18:24:11 +0200

    Clean code

--- FamilyShow/ComposingConverter.cs
+++ FamilyShow/ComposingConverter.cs
@@ -5,37 +5,37 @@ using System.Windows.Data;
 
 namespace Microsoft.FamilyShow
 {
-    public class ComposingConverter : IValueConverter
+  public class ComposingConverter : IValueConverter
+  {
+    #region IValueCOnverter Members
+
+    private List<IValueConverter> converters = new List<IValueConverter>();
+
+    public Collection<IValueConverter> Converters
     {
-        #region IValueCOnverter Members
-
-        private List<IValueConverter> converters = new List<IValueConverter>();
-
-        public Collection<IValueConverter> Converters
-        {
-            get { return new Collection<IValueConverter>(converters); }
-        }
-
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            for (int i = 0; i < converters.Count; i++)
-            {
-                value = converters[i].Convert(value, targetType, parameter, culture);
-            }
-
-            return value;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            for (int i = converters.Count - 1; i >= 0; i--)
-            {
-                value = converters[i].ConvertBack(value, targetType, parameter, culture);
-            }
-
-            return value;
-        }
-
-        #endregion
+      get { return new Collection<IValueConverter>(converters); }
     }
+
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      for (int i = 0; i < converters.Count; i++)
+      {
+        value = converters[i].Convert(value, targetType, parameter, culture);
+      }
+
+      return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      for (int i = converters.Count - 1; i >= 0; i--)
+      {
+        value = converters[i].ConvertBack(value, targetType, parameter, culture);
+      }
+
+      return value;
+    }
+
+    #endregion
+  }
 }
