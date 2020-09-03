edb8c938551e8bbad4c1cf9151336a7b6d62d985
Fred <domaine75@gmail.com>
2020-09-03 18:54:13 +0200

    Clean code

--- FamilyShow/ValueConverters.cs
+++ FamilyShow/ValueConverters.cs
@@ -4,44 +4,43 @@ using System.Windows.Data;
 
 namespace Microsoft.FamilyShow
 {
-    /// <summary>
-    /// This converter is used to show DateTime in short date format
-    /// </summary>
-    public class DateFormattingConverter : IValueConverter
+  /// <summary>
+  /// This converter is used to show DateTime in short date format
+  /// </summary>
+  public class DateFormattingConverter : IValueConverter
+  {
+    #region IValueConverter Members
+
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
     {
-        #region IValueConverter Members
+      if (value != null)
+      {
+        return ((DateTime)value).ToShortDateString();
+      }
 
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-        {
-            if (value != null)
-            {
-                return ((DateTime)value).ToShortDateString();
-            }
-
-            return string.Empty;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            // Ignore empty strings. this will cause the binding to bypass validation.
-            if (string.IsNullOrEmpty((string)value))
-            {
-                return Binding.DoNothing;
-            }
-
-            string dateString = (string)value;
-
-            // Append first month and day if just the year was entered
-            if (dateString.Length == 4)
-            {
-                dateString = "1/1/" + dateString;
-            }
-
-            DateTime date;
-            DateTime.TryParse(dateString, out date);
-            return date;
-        }
-
-        #endregion
+      return string.Empty;
     }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      // Ignore empty strings. this will cause the binding to bypass validation.
+      if (string.IsNullOrEmpty((string)value))
+      {
+        return Binding.DoNothing;
+      }
+
+      string dateString = (string)value;
+
+      // Append first month and day if just the year was entered
+      if (dateString.Length == 4)
+      {
+        dateString = "1/1/" + dateString;
+      }
+
+      _ = DateTime.TryParse(dateString, out DateTime date);
+      return date;
+    }
+
+    #endregion
+  }
 }
