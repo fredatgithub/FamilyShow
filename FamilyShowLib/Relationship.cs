84d25fd6a523df995f4c9145562b767317459183
Fred <domaine75@gmail.com>
2020-10-17 01:03:13 +0200

    Clean code

--- FamilyShowLib/Relationship.cs
+++ FamilyShowLib/Relationship.cs
@@ -4,217 +4,217 @@ using System.Xml.Serialization;
 
 namespace Microsoft.FamilyShowLib
 {
-    #region Relationship classes
+  #region Relationship classes
+
+  /// <summary>
+  /// Describes the kinship between person objects
+  /// </summary>
+  [Serializable]
+  public abstract class Relationship
+  {
+    private RelationshipType relationshipType;
+
+    private Person relationTo;
+
+    // The person's Id will be serialized instead of the relationTo person object to avoid
+    // circular references during Xml Serialization. When family data is loaded, the corresponding
+    // person object will be assigned to the relationTo property (please see app.xaml.cs).
+    private string personId;
+
+    // Store the person's name with the Id to make the xml file more readable
+    private string personFullname;
 
     /// <summary>
-    /// Describes the kinship between person objects
+    /// The Type of relationship.  Parent, child, sibling, or spouse
     /// </summary>
-    [Serializable]
-    public abstract class Relationship
+    public RelationshipType RelationshipType
     {
-        private RelationshipType relationshipType;
-
-        private Person relationTo;
-
-        // The person's Id will be serialized instead of the relationTo person object to avoid
-        // circular references during Xml Serialization. When family data is loaded, the corresponding
-        // person object will be assigned to the relationTo property (please see app.xaml.cs).
-        private string personId;
-
-        // Store the person's name with the Id to make the xml file more readable
-        private string personFullname;
-
-        /// <summary>
-        /// The Type of relationship.  Parent, child, sibling, or spouse
-        /// </summary>
-        public RelationshipType RelationshipType
-        {
-            get { return relationshipType; }
-            set { relationshipType = value; }
-        }
-
-        /// <summary>
-        /// The person id the relationship is to. See comment on personId above.
-        /// </summary>
-        [XmlIgnore]
-        public Person RelationTo
-        {
-            get { return relationTo; }
-            set
-            {
-                relationTo = value;
-                personId = ((Person)value).Id;
-                personFullname = ((Person)value).FullName;
-            }
-        }
-
-        public string PersonId
-        {
-            get { return personId; }
-            set { personId = value; }
-        }
-
-        public string PersonFullname
-        {
-            get { return personFullname; }
-            set { personFullname = value; }
-        }
+      get { return relationshipType; }
+      set { relationshipType = value; }
     }
 
     /// <summary>
-    /// Describes the kinship between a child and parent.
+    /// The person id the relationship is to. See comment on personId above.
     /// </summary>
-    [Serializable]
-    public class ParentRelationship : Relationship
+    [XmlIgnore]
+    public Person RelationTo
     {
-        // The ParentChildModifier are not currently used by the application
-        private ParentChildModifier parentChildModifier;
-        public ParentChildModifier ParentChildModifier
-        {
-            get { return parentChildModifier; }
-            set { parentChildModifier = value; }
-        }
-
-        // Paramaterless constructor required for XML serialization
-        public ParentRelationship() { }
-
-        public ParentRelationship(Person personId, ParentChildModifier parentChildType)
-        {
-            RelationshipType = RelationshipType.Parent;
-            RelationTo = personId;
-            parentChildModifier = parentChildType;
-        }
-
-        public override string ToString()
-        {
-            return RelationTo.Name;
-        }
+      get { return relationTo; }
+      set
+      {
+        relationTo = value;
+        personId = ((Person)value).Id;
+        personFullname = ((Person)value).FullName;
+      }
     }
 
-    /// <summary>
-    /// Describes the kindship between a parent and child.
-    /// </summary>
-    [Serializable]
-    public class ChildRelationship : Relationship
+    public string PersonId
     {
-        // The ParentChildModifier are not currently used by the application
-        private ParentChildModifier parentChildModifier;
-        public ParentChildModifier ParentChildModifier
-        {
-            get { return parentChildModifier; }
-            set { parentChildModifier = value; }
-        }
-
-        // Paramaterless constructor required for XML serialization
-        public ChildRelationship() { }
-
-        public ChildRelationship(Person person, ParentChildModifier parentChildType)
-        {
-            RelationshipType = RelationshipType.Child;
-            RelationTo = person;
-            parentChildModifier = parentChildType;
-        }
+      get { return personId; }
+      set { personId = value; }
     }
 
-    /// <summary>
-    /// Describes the kindship between a couple
-    /// </summary>
-    [Serializable]
-    public class SpouseRelationship : Relationship
+    public string PersonFullname
     {
-        private SpouseModifier spouseModifier;
-        private DateTime? marriageDate;
-        private DateTime? divorceDate;
-        private String marriagePlace;
+      get { return personFullname; }
+      set { personFullname = value; }
+    }
+  }
 
-        public SpouseModifier SpouseModifier
-        {
-            get { return spouseModifier; }
-            set { spouseModifier = value; }
-        }
-
-        public DateTime? MarriageDate
-        {
-            get { return marriageDate; }
-            set { marriageDate = value; }
-        }
-
-        public DateTime? DivorceDate
-        {
-            get { return divorceDate; }
-            set { divorceDate = value; }
-        }
-        public String MarriagePlace
-        {
-            get { return marriagePlace; }
-            set { marriagePlace = value; }
-        }
-
-
-        // Paramaterless constructor required for XML serialization
-        public SpouseRelationship() { }
-
-        public SpouseRelationship(Person person, SpouseModifier spouseType)
-        {
-            RelationshipType = RelationshipType.Spouse;
-            spouseModifier = spouseType;
-            RelationTo = person;
-        }
+  /// <summary>
+  /// Describes the kinship between a child and parent.
+  /// </summary>
+  [Serializable]
+  public class ParentRelationship : Relationship
+  {
+    // The ParentChildModifier are not currently used by the application
+    private ParentChildModifier parentChildModifier;
+    public ParentChildModifier ParentChildModifier
+    {
+      get { return parentChildModifier; }
+      set { parentChildModifier = value; }
     }
 
-    /// <summary>
-    /// Describes the kindship between a siblings
-    /// </summary>
-    [Serializable]
-    public class SiblingRelationship : Relationship
-    {
-        // Paramaterless constructor required for XML serialization
-        public SiblingRelationship() { }
+    // Paramaterless constructor required for XML serialization
+    public ParentRelationship() { }
 
-        public SiblingRelationship(Person person)
-        {
-            RelationshipType = RelationshipType.Sibling;
-            RelationTo = person;
-        }
+    public ParentRelationship(Person personId, ParentChildModifier parentChildType)
+    {
+      RelationshipType = RelationshipType.Parent;
+      RelationTo = personId;
+      parentChildModifier = parentChildType;
     }
 
-    #endregion
-
-    #region Relationships collection
-
-    /// <summary>
-    /// Collection of relationship for a person object
-    /// </summary>
-    [Serializable]
-    public class RelationshipCollection : ObservableCollection<Relationship> { }
-
-    #endregion
-
-    #region Relationship Type/Modifier Enums
-
-    /// <summary>
-    /// Enumeration of connection types between person objects
-    /// </summary>
-    public enum RelationshipType
+    public override string ToString()
     {
-        Child,
-        Parent,
-        Sibling,
-        Spouse
+      return RelationTo.Name;
+    }
+  }
+
+  /// <summary>
+  /// Describes the kindship between a parent and child.
+  /// </summary>
+  [Serializable]
+  public class ChildRelationship : Relationship
+  {
+    // The ParentChildModifier are not currently used by the application
+    private ParentChildModifier parentChildModifier;
+    public ParentChildModifier ParentChildModifier
+    {
+      get { return parentChildModifier; }
+      set { parentChildModifier = value; }
     }
 
-    public enum SpouseModifier
+    // Paramaterless constructor required for XML serialization
+    public ChildRelationship() { }
+
+    public ChildRelationship(Person person, ParentChildModifier parentChildType)
     {
-        Current,
-        Former
+      RelationshipType = RelationshipType.Child;
+      RelationTo = person;
+      parentChildModifier = parentChildType;
+    }
+  }
+
+  /// <summary>
+  /// Describes the kindship between a couple
+  /// </summary>
+  [Serializable]
+  public class SpouseRelationship : Relationship
+  {
+    private SpouseModifier spouseModifier;
+    private DateTime? marriageDate;
+    private DateTime? divorceDate;
+    private String marriagePlace;
+
+    public SpouseModifier SpouseModifier
+    {
+      get { return spouseModifier; }
+      set { spouseModifier = value; }
     }
 
-    public enum ParentChildModifier
+    public DateTime? MarriageDate
     {
-        Natural,
-        Adopted,
-        Foster
+      get { return marriageDate; }
+      set { marriageDate = value; }
     }
 
-    #endregion
+    public DateTime? DivorceDate
+    {
+      get { return divorceDate; }
+      set { divorceDate = value; }
+    }
+    public String MarriagePlace
+    {
+      get { return marriagePlace; }
+      set { marriagePlace = value; }
+    }
+
+
+    // Paramaterless constructor required for XML serialization
+    public SpouseRelationship() { }
+
+    public SpouseRelationship(Person person, SpouseModifier spouseType)
+    {
+      RelationshipType = RelationshipType.Spouse;
+      spouseModifier = spouseType;
+      RelationTo = person;
+    }
+  }
+
+  /// <summary>
+  /// Describes the kindship between a siblings
+  /// </summary>
+  [Serializable]
+  public class SiblingRelationship : Relationship
+  {
+    // Paramaterless constructor required for XML serialization
+    public SiblingRelationship() { }
+
+    public SiblingRelationship(Person person)
+    {
+      RelationshipType = RelationshipType.Sibling;
+      RelationTo = person;
+    }
+  }
+
+  #endregion
+
+  #region Relationships collection
+
+  /// <summary>
+  /// Collection of relationship for a person object
+  /// </summary>
+  [Serializable]
+  public class RelationshipCollection : ObservableCollection<Relationship> { }
+
+  #endregion
+
+  #region Relationship Type/Modifier Enums
+
+  /// <summary>
+  /// Enumeration of connection types between person objects
+  /// </summary>
+  public enum RelationshipType
+  {
+    Child,
+    Parent,
+    Sibling,
+    Spouse
+  }
+
+  public enum SpouseModifier
+  {
+    Current,
+    Former
+  }
+
+  public enum ParentChildModifier
+  {
+    Natural,
+    Adopted,
+    Foster
+  }
+
+  #endregion
 }
